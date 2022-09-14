1921ea5a5f5c7bd1f88003a786fee7e968773ac5
Nikola Đuza <nikolaseap@gmail.com>
2022-09-14 08:53:16 +0200

    Cache resources and use cache-first approach

---
+++ README.md
@@ -0,0 +1,9 @@
+# service-worker-jungle
+
+To run this code locally you'll need to serve it because of the Service Worker spec.
+
+For example, if you have `node` installed, navigate to the folder containing the code and run:
+
+```
+npx lite-server
+```
---
+++ script.js
@@ -0,0 +1,18 @@
+const registerWorker = async () => {
+  if ('serviceWorker' in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("/worker.js");
+      if (registration.installing) {
+        console.log("Service worker installing");
+      } else if (registration.waiting) {
+        console.log("Service worker installed");
+      } else if (registration.active) {
+        console.log("Service worker active");
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
+}
+
+registerWorker()
